d990d4507fd677d24bc205cf950ad91587daaf57
gakhi <gakhi@DESKTOP-DB9ED7V>
2023-01-19 16:10:49 +0530

    i have changed some repo

---
+++ src/test/java/pracGithub/gitHubPractic123456.java
@@ -0,0 +1,12 @@
+package pracGithub;
+
+import org.testng.annotations.Test;
+
+public class gitHubPractic123456 {
+@Test
+public void create()
+{
+	System.out.println("Mission Succesfully");
+	
+}
+}
