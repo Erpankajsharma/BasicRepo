3fd5cb0cf2c078e7a2fc04762c07cf1929f21534
Me <Me@DESKTOP-HM27A8M>
2023-01-20 10:22:44 +0530

    changes comit5555

---
+++ src/test/java/pracGithub/ChangesInGithub.java
@@ -0,0 +1,11 @@
+package pracGithub;
+
+import org.testng.annotations.Test;
+
+public class ChangesInGithub {
+
+	@Test
+	public void changes() {
+		System.out.println("changes sucessfully");
+	}
+}
