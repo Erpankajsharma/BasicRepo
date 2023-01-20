2382317128eda0f8efa4fd72fe0f4f7c85faa765
user <user@DESKTOP-T5MI7TC>
2023-01-20 15:16:27 +0530

    rebay commit

---
+++ src/test/java/pracGithub/copy2/UpdateMadhu.java
@@ -0,0 +1,11 @@
+package pracGithub.copy2;
+
+import org.testng.annotations.Test;
+
+public class UpdateMadhu {
+	@Test
+	public void update()
+	{
+		System.out.println("Updated successfully");
+	}
+}
---
+++ src/test/java/pracGithub/copy2/demomadhu.java
@@ -0,0 +1,11 @@
+package pracGithub.copy2;
+
+import org.testng.annotations.Test;
+
+public class demomadhu {
+	@Test
+	public void repo()
+	{
+		System.out.println("repo");
+	}
+}
